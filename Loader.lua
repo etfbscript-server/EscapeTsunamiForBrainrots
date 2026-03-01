f860bf0eabd65b681d44ad755301295647294561
etfbscript-server <ashiiwng08@gmail.com>
2026-03-01 16:37:53 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,8 +1,8 @@
 local function s1()
 end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrots/refs/heads/main/Loader.lua"))()
 local function s2()
 end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/etfbscript-server/EscapeTsunamiForBrainrots/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
 task.spawn(s1)
 task.spawn(s2)
